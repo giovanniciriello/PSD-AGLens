713eff8163b8233a6688a092dce715a11f707839
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2021-07-19 18:57:52 +0200

    updates

--- client/functions/check-frame-availability.js
+++ client/functions/check-frame-availability.js
@@ -7,12 +7,19 @@ var http = new HttpClient();
 const { baseUrls } = require('../config');
 
 exports.checkFrameAvailability = async ({ task, taskService }) => {
-  console.log('controllo disponibilità frame n. ', task.variables.get('frame_id'));
+  console.log(`Checking for frame availability in supplier warehouse for item ${task.variables.get('frame_id')}`);
 
   http.get(baseUrls.frameService + 'check-frame-availability/' + task.variables.get('frame_id'), async (data) => {
-    const processVariables = new Variables()
-      .set('availability_supplier', data.available)
-      .set('days_to_deliver', data.daysToDeliver);
+    const processVariables = new Variables();
+
+    processVariables.set('availability_supplier', data.available);
+
+    if (data.available) {
+      processVariables.set('price', data.price);
+      processVariables.set('days_to_deliver', data.daysToDeliver);
+    }
+
+    // console.log(processVariables.getAll());
 
     // Complete the task
     await taskService.complete(task, processVariables);
--- services/apis/framesupplier.js
+++ services/apis/framesupplier.js
@@ -27,20 +27,24 @@ const framesDatabase = {
     name: 'Persol light white eyeglass',
     available: true,
     daysToDeliver: 8,
+    price: 185,
   },
   'persol-dark': {
     name: 'Persol dark black eyeglass',
     available: true,
     daysToDeliver: 5,
+    price: 150,
   },
   'rayban-vintage': {
     name: 'Rayban vintage style eyeglass',
     available: true,
     daysToDeliver: 25,
+    price: 120,
   },
   'rayban-modern': {
     name: 'Rayban modern style eyeglass',
     available: true,
     daysToDeliver: 18,
+    price: 110,
   },
 };
