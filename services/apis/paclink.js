4a9a891a543d20013669be69c705284e4cc20acc
AndreaBovoPolimi <andrea.bovo@mail.polimi.it>
2021-07-21 18:30:30 +0200

    small changes

--- services/apis/paclink.js
+++ services/apis/paclink.js
@@ -1,17 +1,6 @@
 exports.createShippingOrder = (req, res) => {
-  console.log(req.body);
-
-  // packlink does not support too large packs...
-  const pack = req.body;
-  if (pack.height > 20 || pack.width > 10 || pack.depth > 8) {
-    res.json({
-      success: false,
-      message: '⚠️📦 Pack too large!',
-    });
-  } else {
-    res.json({
-      success: true,
-      message: '✅ order successful issued! 📦 The pack will be delivered soon!',
-    });
-  }
+  res.json({
+    success: true,
+    message: '✅ order successful issued! 📦 The pack will be delivered soon!',
+  });
 };
